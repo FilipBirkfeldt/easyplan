0fb2e7c4a6f30e3b055a6f7fe309e20e387a4ffe
FilipBirkfeldt <filip.birkfeldt@gmail.com>
2021-03-18 21:30:57 +0100

    la till en dictionary med totaler av poäng o hur mkt en person har kvar

--- checkPoints_graduation/calculatePoints.py
+++ checkPoints_graduation/calculatePoints.py
@@ -3,33 +3,85 @@ import numpy as np
 
 def readInData(): 
     "Reads in the data needed - Currently courses at M"
-    courses_df = pd.read_excel('checkPoints_graduation/coursesM.xlsx')
+    df_13 = pd.read_excel('checkPoints_graduation/data_Poäng/dataM.xlsx', sheet_name='1-3')
+    df_specialiseringar = pd.read_excel('checkPoints_graduation/data_Poäng/dataM.xlsx', sheet_name='Specialiseringar')
+    df_ValfriaM = pd.read_excel('checkPoints_graduation/data_Poäng/dataM.xlsx', sheet_name='Valfria_M')
+
+    courses_df = df_13.append(df_specialiseringar)
+    courses_df = courses_df.append(df_ValfriaM)
+
     courses_df.rename(columns={'Kurskod':'Kurskod'}, inplace=True)
     courses_df['Poäng'] = courses_df['Poäng'].fillna(0)
     courses_df['Poäng'] = courses_df['Poäng'].apply(lambda x:float(x))
     return courses_df
 
-def getActualDf(courses_df, course_list):
+def __getPersonDf(courses_df, course_list):
     "Return the point"
     course_list = list(course_list)
     boolean_series = courses_df['Kurskod'].apply(lambda x:x in course_list)
     person_df = courses_df[boolean_series]
     return person_df
 
-def calculateAllPoints(dfPerson) -> float: 
+def __calculateAllPoints(dfPerson) -> float: 
     "Calculates points for the person"
     return dfPerson['Poäng'].sum()
 
-def calculate_A_points(dfPerson) -> float: 
+def __calculate_A_points(dfPerson) -> float: 
     "Calculates all A-Points"
-    return dfPerson[dfPerson['Nivå'].str.contains('A')]['Poäng'].sum()
+    return dfPerson[dfPerson['Nivå'].str.contains('A', na=False)]['Poäng'].sum()
 
-def getPoints_specialization(dfPerson, speci)-> float: 
-    print('RÖV')
+def __getPoints_specialization(dfPerson, speci)-> float: 
+    "Returns the points for the given specialization"
+    return dfPerson[dfPerson.Typ == speci]['Poäng'].sum()
 
 
-courses_df = readInData()
-course_list = courses_df.Kurskod.iloc[0:20]
-person_df = getActualDf(courses_df, course_list)
-all_points = calculateAllPoints(person_df)
-a_points = calculate_A_points(person_df)
+def getAllPointsDict(progam, specialisering, course_list) -> dict:
+    "Returns a dictionary for the specific person, allP, A-P, spec-P"
+    courses_df = readInData()
+    course_list = courses_df.Kurskod.values
+    person_df = __getPersonDf(courses_df, course_list)
+
+    all_points = __calculateAllPoints(person_df)
+    a_points = __calculate_A_points(person_df)
+    spec_points = __getPoints_specialization(person_df, 'Mekatronik')
+
+    #Creates the dictionary for the student 
+    personDict = {
+        'Total_Points' : all_points, 
+        'A_Points' : a_points,
+        'Spec_Points' : spec_points
+    }
+    return personDict
+
+def addPointsLeft_toDict(personDict) -> dict: 
+    "Adds points left to graduation"
+    aPoints_req = 45
+    totalPoints_req = 300
+    specPoints_req = 45
+
+    # Lägger till keys (_left) som visar hur många poäng personen behöver för Graduation
+    keys_dict = list(personDict.keys())
+    for key in keys_dict: 
+        if 'Spec' in key: 
+            points_left = specPoints_req - personDict.get(key)
+        elif 'Total' in key: 
+            points_left = totalPoints_req - personDict.get(key)
+        if 'A' in key: 
+            points_left = aPoints_req - personDict.get(key)
+
+        personDict.update({(key+'_left'): points_left})
+    
+    return personDict
+
+
+def presentPerson(): 
+    "Bara för att visa er var vi är so far"
+    personDict = getAllPointsDict('M', 'Mekatronik', ['kommer va chill när den här fungerar'])
+    personDict = addPointsLeft_toDict(personDict)
+    print()
+    for key in personDict.keys(): 
+        if 'left' in key: 
+            continue
+        print('\n', key, ' - ', personDict.get(key), '  left: ', personDict.get(key+'_left'))
+
+presentPerson()
