8b59ed14ac618ccdd98cfd4bcee9b751ceec8208
Christian Sidemo <idemo96@gmail.com>
2021-03-23 09:52:53 +0100

    User register form

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FieldList, FormField, Form
+from wtforms import StringField, SubmitField, FieldList, FormField, Form, PasswordField
+from wtforms.validators import Length, EqualTo, DataRequired, Email
 
 
 class courseField(FlaskForm):
@@ -11,7 +12,13 @@ class courseList(FlaskForm):
     submit = SubmitField('Beräkna')
 
 
-class User(FlaskForm):
-    name = StringField(label = 'Name', nullable = False, unique = False)
-    email_address = StringField(label = 'Email Adress', nullable = False, unique = True)
-    password = StringField(label = 'Password')
+class Registrator(FlaskForm):
+
+    def validate_email_address(self, email_adress_to_check):
+        pass
+
+    FirstName = StringField(label = 'Name', validators = [DataRequired, Length(min = 2, max = 20)])
+    LastName = StringField(label = 'Name', validators = [DataRequired, Length(min = 2, max = 20)])
+    email_address = StringField(label = 'Email Adress', validators = [Email(), DataRequired ])
+    password = PasswordField(label = 'Password', validators = [Length(min = 3, max = 15)])
+    passwordConfirmation = PasswordField(label = 'Confirm password', validators = [EqualTo(password)])
