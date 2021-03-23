153edf3a872a780a5f3d9298c0f53036cbf1ace1
FilipBirkfeldt <filip.birkfeldt@gmail.com>
2021-03-23 18:30:01 +0100

    DB-pill

---
+++ checkPoints_graduation/userTable.sql
@@ -0,0 +1,10 @@
+USE KURSER_M; 
+
+CREATE TABLE user_tableTable ( 
+userID MEDIUMINT PRIMARY KEY, 
+userMail TINYTEXT, 
+userPassWord TINYTEXT,
+firstName TINYTEXT, 
+program TINYTEXT, 
+specialisering TINYTEXT
+);
